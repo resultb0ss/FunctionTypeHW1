c2f23c32c33b414fa52c17f5bde1291089afc46e
Admin <reteamboss@gmail.com>
2024-09-01 20:27:33 +0300

    Задание 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+
+
+
+    println(factroial(3))
+
+}
+
+fun factroial(n: Int): Int {
+    require(n >= 0)
+    return if (n<=1){1}
+    else {
+        n * factroial(n - 1)
+
+    }
 }
